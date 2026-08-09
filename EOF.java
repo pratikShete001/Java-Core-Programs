f2668a9cc6a8670a8db7430dade8511125ad1957
Pratik Shete <pratikshete827@gmail.com>
2026-08-09 10:40:04 +0530

    Add files via upload

---
+++ EOF.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int lineNumber = 1;
+
+        // Read until EOF
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            System.out.println(lineNumber + " " + line);
+            lineNumber++;
+        }
+        sc.close();
+    }
+}
